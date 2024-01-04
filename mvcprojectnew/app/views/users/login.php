d0fbaac9fe65d20f1e1d80349d68d0b96c3b2ef5
MuhdAnass <anaspikri13@gmail.com>
2024-01-05 01:52:28 +0800

    update

--- mvcprojectnew/app/views/users/login.php
+++ mvcprojectnew/app/views/users/login.php
@@ -64,7 +64,7 @@
                                 <h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
                                 <!--end::Title-->
                                 <!--begin::Subtitle-->
-                                <div class="text-gray-500 fw-semibold fs-6">Your  Campaigns</div>
+                                <div class="text-gray-500 fw-semibold fs-6">Youth Ventures Data Management System</div>
                                 <!--end::Subtitle=-->
                             </div>
                             <!--begin::Heading-->
@@ -131,21 +131,20 @@
             <!--end::Body-->
             <!--begin::Aside-->
             <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
-                style="background-image: url(<?php echo URLROOT ?>/public/assets/media/misc/auth-bg.png)">
+                style="background-image: url(<?php echo URLROOT ?>/public/assets/media/misc/yvback.JPG)">
                 <!--begin::Content-->
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!--begin::Logo-->
-                    <a href="index.html" class="mb-0 mb-lg-12">
-                        <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/custom-1.png" class="h-60px h-lg-75px" />
+                    <a href="index.html" class="mb-0 mb-lg-5">
+                        <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/yvlogo.png" class="h-100px h-lg-300px" style="margin-top: -20px;"/>
                     </a>
                     <!--end::Logo-->
                     <!--begin::Image-->
                     <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
-                        src="<?php echo URLROOT ?>/public/assets/media/misc/auth-screens.png" alt="" />
+                        src="<?php echo URLROOT ?>/public/assets/media/misc/YouthVenture.jpeg" alt="" />
                     <!--end::Image-->
                     <!--begin::Title-->
-                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and
-                        Productive</h1>
+                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7"></h1>  <!-- Tambah ayat kalau nak-->
                     <!--end::Title-->
                     <!--begin::Text-->
                     <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
