6eaa9abd6910e3362c52d0d7946e8a527e531563
Navachander1 <navachander@graduate.utm.my>
2024-01-07 23:38:34 +0800

    Update mvcframework.sql

--- mvcprojectnew/mvcframework.sql
+++ mvcprojectnew/mvcframework.sql
@@ -151,3 +151,109 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Table structure for table `badges`
+--
+
+CREATE TABLE `badges` (
+  `badge_id` int(11) NOT NULL,
+  `badge_desc` varchar(255) NOT NULL,
+  `badge_img` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `badge_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `badges`
+--
+
+INSERT INTO `badges` (`badge_id`, `badge_desc`, `badge_img`, `user_id`, `badge_title`) VALUES
+(2, 'dfsfd', 'Untitled Diagram.jpg', 1, 'asdas'),
+(3, 'asd', '06D439C5-6E12-447F-87BB-77750DB927BE.png', 3, 'asd'),
+(4, 'great 2', 'images/badges/lol2@gmail.com/659abe011aba64.77854818.png', 3, 'great'),
+(5, 'sdv szvsvsv', 'images/badges/lol2@gmail.com/659ac19dd2b956.68060309.png', 3, 'sdvsdv');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `badges`
+--
+ALTER TABLE `badges`
+  ADD PRIMARY KEY (`badge_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `badges`
+--
+ALTER TABLE `badges`
+  MODIFY `badge_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Table structure for table `activities`
+--
+
+CREATE TABLE `activities` (
+  `act_id` int(11) NOT NULL,
+  `act_title` varchar(255) NOT NULL,
+  `act_desc` text NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `act_startdate` date NOT NULL DEFAULT '2023-01-01',
+  `act_enddate` date NOT NULL DEFAULT '2023-01-01',
+  `act_starttime` time NOT NULL,
+  `act_endtime` time NOT NULL,
+  `act_venue` varchar(255) NOT NULL,
+  `act_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`act_id`, `act_title`, `act_desc`, `user_id`, `act_startdate`, `act_enddate`, `act_starttime`, `act_endtime`, `act_venue`, `act_image`) VALUES
+(1, 'yolo', 'yololo', '1', '2023-01-01', '2023-01-01', '00:00:00', '00:00:00', '', ''),
+(2, 'Entrepreneurship', 'DAQ', '1', '2023-01-02', '2023-01-20', '18:00:00', '16:00:00', 'DAC', 'event management subsystem class diagram (1).jpg'),
+(4, 'matrix', 'weaponised virtue', '1', '2023-01-10', '2023-01-11', '12:00:00', '16:00:00', 'romania', 'images/activities/lol2@gmail.com/659ab77543bed2.26885543.jpg'),
+(5, 'test time2', 'test time2', '1', '2023-01-01', '2023-01-01', '00:00:00', '00:00:00', '', ''),
+(7, 'test time4', 'test time4', '1', '2023-01-01', '2023-01-01', '00:00:00', '00:00:00', '', ''),
+(8, 'test time5', 'test time5', '1', '2023-01-01', '2023-01-01', '00:00:00', '00:00:00', '', ''),
+(9, 'test time6', 'test time6', '1', '2023-01-01', '2023-01-01', '00:00:00', '00:00:00', '', ''),
+(10, 'zsrhzsrhb', 'zsdrhbzer', '1', '2023-01-01', '2023-01-01', '00:00:00', '00:00:00', '', ''),
+(11, 'zsegzsdvcxcx', 'zsdfdvzvdv', '1', '2023-01-01', '2023-01-01', '00:00:00', '00:00:00', '', ''),
+(12, 'marathon', 'marathon2.0', '1', '2023-01-26', '2023-01-26', '16:00:00', '15:00:00', 'k9k10', 'images/activities/lol2@gmail.com/659ab73a083823.53161648.jpg'),
+(13, '123asdasda', 'n28', '1', '2023-01-02', '2023-01-03', '15:00:00', '16:00:00', 'n28a', 'event management subsystem class diagram (1).jpg'),
+(14, 'testnew1', 'testnew1', '1', '2023-12-14', '2023-12-15', '04:25:00', '05:25:00', 'testnew1', 'Untitled Diagram (2).jpg'),
+(15, '213', 'wedewd', '1', '2023-12-21', '2023-12-06', '22:35:00', '22:35:00', 'wwedew', 'Untitled Diagram.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`act_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `act_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
